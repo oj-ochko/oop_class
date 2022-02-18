c81a4dfa45f66720e4aa555e4a26e86a7c1a95f1
oj-ochko <73417322+oj-ochko@users.noreply.github.com>
2022-02-18 11:02:32 +0800

    Add files via upload

---
+++ week_1/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+    cout << "what is your name: ";
+    cin >> name;
+    cout << "hi " << name << endl;
+    cout << "how old are you: ";
+    cin >> age;
+    cout << name << " is " << age << endl;
+}
